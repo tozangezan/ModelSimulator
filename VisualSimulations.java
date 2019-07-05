e7b6f60d996a4d721032234b057f18762aaecb28
Ikumi Hide <kiyotokorokoro@gmail.com>
2019-07-05 15:33:31 +0900

    add

--- VisualSimulations.java
+++ VisualSimulations.java
@@ -11,26 +11,39 @@ public  class VisualSimulations{
 	SecureRandom r1;
 	int[] dr={1,1,1,0,0,-1,-1,-1};
 	int[] dc={1,0,-1,1,-1,1,0,-1};
+	static boolean debug;
+	static boolean regular;
 
 	// model
 	int nPartition;
 	double K;
 	double r;
 	double C0;
+	double d;
+	double M;
 
 	// field
 	int H,W;
+	int O;
 	int S;
 	int T;
 	int maxT;
 	double[][] field;
+	double[][] new_field;
+	
 	double[][] disturbance;
 	int[] sheep_row;
 	int[] sheep_col;
 	int[] sheep_pressure;
 	
+	// log
+
+
 	boolean isInside(int r, int c) {
 		return (r >= 0 && r < H && c >= 0 && c < W);
+	}
+	boolean isInsideFence(int r, int c) {
+		return (r >= O && r < H-O && c >= O && c < W-O);
 	}
 
 	void generate(String seedStr){
@@ -40,15 +53,19 @@ public  class VisualSimulations{
 			r1.setSeed(seed);
 			H = 50;
 			W = 50;
-			S = 50;
+			S = 200;
+			O = 5;
 			T = 0;
 
 			nPartition = 1000;
-			r = 0.5;
+			r = 0.3;
 			K = 100;
-			C0 = 0;
-			maxT = 500;
+			C0 = 0.1;
+			d = 0.3;
+			M = 20;
+			maxT = 5000;
 			field=new double[H][W];
+			new_field=new double[H][W];
 			disturbance=new double[H][W];
 			sheep_row=new int[S];
 			sheep_col=new int[S];
@@ -56,16 +73,23 @@ public  class VisualSimulations{
 			// TODO: 何か generate するべきものをする
 			for(int i=0;i<H;i++){
 				for(int j=0;j<W;j++){
-					field[i][j]=r1.nextDouble()*K;
+				//	field[i][j]=r1.nextDouble()*K;
+					field[i][j]=K;
 					disturbance[i][j]=C0;
 					// nextGaussianでもいいかも
 				}
 			}
 
 			for(int i=0;i<S;i++){
-				sheep_row[i]=r1.nextInt(H);
-				sheep_col[i]=r1.nextInt(W);
-				sheep_pressure[i]=30;
+				if(regular) {
+					int index=(H-2*O)*(W-2*O)*i/S;
+					sheep_row[i]=index/(W-2*O)+O;
+					sheep_col[i]=index%(W-2*O)+O;
+				}else{
+					sheep_row[i]=r1.nextInt(H-2*O)+O;
+					sheep_col[i]=r1.nextInt(W-2*O)+O;
+				}
+				sheep_pressure[i]=50;
 			}
 
 			for(int i=0;i<S;i++){
@@ -88,27 +112,63 @@ public  class VisualSimulations{
 				T++;
 				try{Thread.sleep(del);}
 				catch(Exception e){}
-				for(int i=0;i<H;i++){
-					for(int j=0;j<W;j++){
-						for(int k=0;k<nPartition;k++){
+				for(int k=0;k<nPartition;k++){
+					for(int i=0;i<H;i++){
+						for(int j=0;j<W;j++){
 							double dt=1.0/nPartition;
 							double x=field[i][j];
-							double dx=x*r*(1-x/K)-disturbance[i][j]*x*x/(1+x*x);
-							field[i][j]+=dt*dx;
+							for(int l=0;l<8;l++){
+								if(isInside(i+dr[l],j+dc[l])){
+									x+=field[i+dr[l]][j+dc[l]]*d/nPartition/8;
+								}
+							}
+							x-=field[i][j]*d/nPartition;
+							double dx=x*r*(1-x/K)-disturbance[i][j]*x*x/(M+x*x);
+
+							new_field[i][j]=field[i][j]+dt*dx;
 						}
+					}
+					for(int i=0;i<H;i++){
+						for(int j=0;j<W;j++){
+							field[i][j]=new_field[i][j];
+						}
+					}
+				}
+				if(debug){
+					for(int i=0;i<H;i++){
+						for(int j=0;j<W;j++){
+							if(j>0)System.out.print(" ");
+							System.out.print(field[i][j]);
+						}
+						System.out.println();
 					}
 				}
 				draw();
 				for(int i=0;i<S;i++){
-					int direction=r1.nextInt(8);
-					int tr=sheep_row[i]+dr[direction];
-					int tc=sheep_col[i]+dc[direction];
-					if(isInside(tr,tc)){
-						disturbance[sheep_row[i]][sheep_col[i]]-=sheep_pressure[i];
-						sheep_row[i]=tr;
-						sheep_col[i]=tc;
-						disturbance[sheep_row[i]][sheep_col[i]]+=sheep_pressure[i];
+					int tr,tc;
+					if(regular){
+						tr=sheep_row[i];
+						tc=sheep_col[i]+1;
+						if(!isInsideFence(tr,tc)){
+							tr++;
+							tc=O;
+							if(!isInsideFence(tr,tc)){
+								tr=O;
+							}
+						}
+					}else{
+						int direction=r1.nextInt(8);
+						tr=(sheep_row[i]+dr[direction]+H)%H;
+						tc=(sheep_col[i]+dc[direction]+W)%W;
+						if(!isInsideFence(tr,tc)){
+							tr=sheep_row[i];
+							tc=sheep_col[i];
+						}
 					}
+					disturbance[sheep_row[i]][sheep_col[i]]-=sheep_pressure[i];
+					sheep_row[i]=tr;
+					sheep_col[i]=tc;
+					disturbance[sheep_row[i]][sheep_col[i]]+=sheep_pressure[i];
 				}
 			}
 
@@ -128,7 +188,7 @@ public  class VisualSimulations{
 	}
 	void DrawBoard(){
 		if(cacheBoard == null) {
-			cacheBoard = new BufferedImage(W*SZ+120,H*SZ+40,BufferedImage.TYPE_INT_RGB);
+			cacheBoard = new BufferedImage(W*SZ+360,H*SZ+40,BufferedImage.TYPE_INT_RGB);
 		}
 		Graphics2D g2 = (Graphics2D)cacheBoard.getGraphics();
 		// background
@@ -144,17 +204,35 @@ public  class VisualSimulations{
 			g2.setColor(new Color(0.0f, 1.0f, 0.0f, Math.min(1.0f, Math.max(0.0f,(float)(cache[i][j]/K)))));
 			g2.fillRect(j * SZ + 1, i * SZ + 1, SZ - 1, SZ - 1);
 		}
-
+		for(int j=0;j<S;j++){
+			g2.setColor(new Color(1.0f, 0.0f, 0.0f, 0.5f));
+			g2.fillRect(sheep_col[j] * SZ + 1, sheep_row[j] * SZ + 1, SZ - 1, SZ - 1);
+		}
 		// lines between cells
 		g2.setColor(Color.BLACK);
 		for (int i = 0; i <= H; i++)
 			g2.drawLine(0,i*SZ,W*SZ,i*SZ);
 		for (int i = 0; i <= W; i++)
 			g2.drawLine(i*SZ,0,i*SZ,H*SZ);
+		g2.setStroke(new BasicStroke(3.0f));
+		g2.drawLine(O*SZ,O*SZ,(W-O)*SZ,O*SZ);
+		g2.drawLine(O*SZ,(H-O)*SZ,(W-O)*SZ,(H-O)*SZ);
+		g2.drawLine(O*SZ,O*SZ,O*SZ,(H-O)*SZ);
+		g2.drawLine((W-O)*SZ,O*SZ,(W-O)*SZ,(H-O)*SZ);
 
 		g2.dispose();
 	}
-	
+	void DrawGraph(){
+		if(cacheBoard == null) {
+			cacheBoard = new BufferedImage(W*SZ+360,H*SZ+40,BufferedImage.TYPE_INT_RGB);
+		}
+		Graphics2D g2 = (Graphics2D)cacheBoard.getGraphics();
+		// background
+		g2.setColor(Color.BLACK);
+		g2.setStroke(new BasicStroke(3.0f));
+
+		g2.dispose();
+	}
 	static BufferedImage deepCopy(BufferedImage source) {
 		BufferedImage b = new BufferedImage(source.getWidth(), source.getHeight(), source.getType());
 		Graphics g = b.getGraphics();
@@ -166,6 +244,7 @@ public  class VisualSimulations{
 	public class Vis extends JPanel implements WindowListener {
 		public void paint(Graphics g){
 			DrawBoard();
+			// DrawGraph();
 
 			BufferedImage bi = deepCopy(cacheBoard);
 			Graphics2D g2 = (Graphics2D)bi.getGraphics();
@@ -175,12 +254,16 @@ public  class VisualSimulations{
 			FontMetrics fm = g2.getFontMetrics();
 			g2.drawString(String.format("T = %d", T), W*SZ+25, 110+fm.getHeight());
 
-			g.drawImage(bi,0,0,W*SZ+120,H*SZ+40,null);
+			g.drawImage(bi,0,0,W*SZ+360,H*SZ+40,null);
 		}
 		public Vis(){}
-		public void windowClosing(WindowEvent e){ }
+		public void windowClosing(WindowEvent e){
+			System.exit(0);
+		}
 		public void windowActivated(WindowEvent e) { }
-		public void windowDeactivated(WindowEvent e) { }
+		public void windowDeactivated(WindowEvent e) {
+			System.exit(0);
+		}
 		public void windowOpened(WindowEvent e) { }
 		public void windowClosed(WindowEvent e) {
 			System.exit(0); 
@@ -202,6 +285,8 @@ public  class VisualSimulations{
 		String seed = "1";
 		del = 100;
 		SZ = 15;
+		debug = false;
+		regular = false;
 		for(int i=0;i<args.length;i++){
 			if(args[i].equals("-seed"))
 				seed = args[++i];
@@ -209,6 +294,10 @@ public  class VisualSimulations{
 				SZ = Integer.parseInt(args[++i]);
 			if(args[i].equals("-delay"))
 				del = Integer.parseInt(args[++i]);
+			if(args[i].equals("-debug"))
+				debug = true;
+			if(args[i].equals("-regular"))
+				regular = true;
 		}
 		VisualSimulations vis = new VisualSimulations(seed);
 	}
